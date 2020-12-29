6d19e47fc00ac4aed9126960fe8ea34935e58d90
alexkhazzam <alex722khazzam@gmail.com>
2020-12-29 15:39:47 -0500

    enter the commit message for your changes. Lines starting

--- 10Challenge.cpp
+++ 10Challenge.cpp
@@ -26,9 +26,9 @@ int *apply_all(const int array1[], size_t array1_size, const int array2[], size_
     return new_arr_ptr;
 }
 
-void print(const int array[], size_t array_size)
+void print(const int array[], size_t array_size, unsigned int array_num)
 {
-    cout << "[";
+    cout << "\nArray " << array_num << ": [";
     for (size_t i{0}; i <= (array_size - 1); ++i)
         cout << " " << array[i] << " ";
     cout << "]";
@@ -42,15 +42,13 @@ int main()
     int array1[]{1, 2, 3, 4, 5};
     int array2[]{10, 20, 30};
 
-    cout << "Array 1: ";
-    print(array1, array1_size);
-
-    cout << "\nArray 2: ";
-    print(array2, array2_size);
+    print(array1, array1_size, 1);
+    print(array2, array2_size, 2);
 
     int *results = apply_all(array1, 5, array2, 3);
-    cout << "\nArray 3: ";
-    print(results, 15);
+    print(results, 15, 3);
+
+    delete[] results;
 
     return 0;
 }
